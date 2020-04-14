e9cec83ddca6bb3cf1bc5448f43963c04603a948
nkumarsubcodevs <navneet.kumar@subcodevs.com>
2020-04-14 11:36:57 +0530

    Latest Code

--- services/PageService.js
+++ services/PageService.js
@@ -12,7 +12,6 @@ const now = new Date();
 class PageService
 {
 	// get single note detail
-
 	async getSinglePage(pageData, callback) {
 		const response = await pageObject.findOne({ where: { id: pageData.page_id,  status:1 } });
 		callback(null, response);
@@ -23,12 +22,14 @@ class PageService
 		const response = await pageObject.findAndCountAll({offset: paginationData.offset, limit: paginationData.limit});
 		callback(null,response);
 	}
+
 	//get single page record
 	async getSinglePageRecord(page_id,callback) {
-		const response = await pageObject.findOne({where:{id: page_id}});
+		const response = await pageObject.findAll({where:{id: page_id}});
 		callback(null,response);
 	}
 	
+	//update page details
 	async updatepageDetails(pageData, callback){
 		const response=await pageObject.update({title:pageData.title,status:pageData.status,update_time: current_datetime.format(now, 'YYYY-MM-DD hh:mm:ss')}, { where: { id: pageData.id }});
 		callback(null, response);
@@ -36,9 +37,7 @@ class PageService
 	
 }
 	
-		// project will be the first entry of the Projects table with the title 'aProject' || null
-	  
-
+		
 module.exports = PageService;
 
 
